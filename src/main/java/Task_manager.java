42ccf5efc13d59bfadb1b66861d21f6e92f0bfed
piotrorzelpioro@gmail.com <piotrorzelpioro@gmail.com>
2021-06-03 15:42:36 +0200

    pierwsza działająca wersja

--- src/main/java/Task_manager.java
+++ src/main/java/Task_manager.java
@@ -1,6 +1,9 @@
+import org.apache.commons.lang3.ArrayUtils;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -32,7 +35,8 @@ public class Task_manager {
     }
 
     public static void displayMenu(){
-        System.out.println("Please select an option:");
+        System.out.println(ConsoleColors.BLUE + "Please select an option:");
+        System.out.print(ConsoleColors.RESET);
         System.out.println("add");
         System.out.println("remove");
         System.out.println("list");
@@ -84,24 +88,111 @@ public class Task_manager {
                 taskListArr[i][j] = taskListArr[i + 1][j];
             }
         }
-        taskListArr = Arrays.copyOf(taskListArr, taskListArr.length - 1);
+        taskListArr = ArrayUtils.remove(taskListArr, taskListArr.length-1);
+        //taskListArr = Arrays.copyOf(taskListArr, taskListArr.length - 1); //metoda alternatywna do poprzedniej linii
         return taskListArr;
     }
 
     public static String[][] addTask(String[][]taskListArr){
+        String task = "";
+        String date = "";
+        String important = "";
         Scanner scan = new Scanner(System.in);
+
         System.out.println("Please add task");
-        System.out.println("In the format 'task, date(year-mm-day), is important(true or false)'");
-        String[] parts = scan.nextLine().split(", ");
-        taskListArr = Arrays.copyOf(taskListArr, taskListArr.length + 1);
+        task = scan.nextLine();
 
-        taskListArr[taskListArr.length-1] = new String[parts.length];
+        date = dateCheck();
 
-        for (int i=0; i<parts.length; i++){
-            taskListArr[taskListArr.length-1][i] = parts[i];
+        System.out.println("Is the task important?(true/false)");
+        important = scan.nextLine();
+        while (!(important.equals("true") || important.equals("false"))){
+            System.out.println("Wrong value!");
+            System.out.println();
+            System.out.println("Is the task important?(true/false)");
+            important = scan.nextLine();
         }
+        taskListArr = Arrays.copyOf(taskListArr, taskListArr.length + 1);
+        taskListArr[taskListArr.length-1] = new String[3];
+        taskListArr[taskListArr.length-1][0] = task;        //to mogę dodać za pomocą pętli, ale w tym przypadku kod byłby dłuższy
+        taskListArr[taskListArr.length-1][1] = date;
+        taskListArr[taskListArr.length-1][2] = important;
+
         return taskListArr;
     }
+
+    public static String dateCheck (){          //Tą część próbowałem zrobić za pomocą metody LocalData.isSuported(), ale nie wyszło
+        Scanner scan = new Scanner(System.in);
+        LocalDate localDate = LocalDate.now();
+        String date = "";
+        int year = 1000, month = 1, day = 1;
+        LocalDate inputDate = LocalDate.of(year, month, day);
+
+        while (inputDate.isBefore(localDate)){
+            System.out.println("Please add year");
+            year = Integer.parseInt(scan.nextLine());
+            while(year > 999999999 || year < -999999999){ //takie wartości znalazłem w opisie metod "LocalData"
+                System.out.println("Invalid year");
+                System.out.println("Please add year");
+                year = Integer.parseInt(scan.nextLine());
+            }
+
+            System.out.println("Please add month");
+            month = Integer.parseInt(scan.nextLine());
+            while(month > 12 || month < 1){
+                System.out.println("Invalid month");
+                System.out.println("Please add month");
+                month = Integer.parseInt(scan.nextLine());
+            }
+
+            boolean goodDay = false;
+            while(!goodDay){
+                System.out.println("Please add day");
+                day = Integer.parseInt(scan.nextLine());
+                switch (month){
+                    case 1:
+                    case 3:
+                    case 5:
+                    case 7:
+                    case 8:
+                    case 10:
+                    case 12:
+                        if (day > 0 && day < 32){
+                            goodDay = true;
+                        }
+                        break;
+                    case 4:
+                    case 6:
+                    case 9:
+                    case 11:
+                        if (day > 0 && day < 31){
+                            goodDay = true;
+                        }
+                        break;
+                    case 2:
+                        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)){
+                            if (day > 0 && day < 30){
+                                goodDay = true;
+                            }
+                        }
+                        else if (day > 0 && day < 29){
+                                goodDay = true;
+                            }
+                }
+                if (goodDay == false){
+                    System.out.println("Invalid day");
+                }
+            }
+
+            inputDate = LocalDate.of(year, month, day);
+            if (inputDate.isBefore(localDate)){
+                System.out.println("This date has already been");
+            }
+        }
+        date = String.join("-", year+"", month+"", day+"");
+        return date;
+    }
+
     public static void exit(String[][]taskListArr, String filename){
         String input = new String();
         try(PrintWriter PrintWriter = new PrintWriter(filename)) {
@@ -112,7 +203,8 @@ public class Task_manager {
         }catch (FileNotFoundException e){
             e.printStackTrace();
         }
-        System.out.println("bye bye");
+        System.out.println(ConsoleColors.RED + "bye bye");
+        System.out.print(ConsoleColors.RESET);
         System.exit(0);
     }
 }
